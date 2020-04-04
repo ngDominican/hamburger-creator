8b40b1ab9179f31152f747abf377b22a6bc77d8e
Ronel Pantaleon <r.pantaleon@castilloss.com>
2020-04-04 15:43:17 -0400

    Adding Google Analytics and Updating to Angular v9

--- src/app/features/creator/ingredients-viewer/ingredients-viewer.component.ts
+++ src/app/features/creator/ingredients-viewer/ingredients-viewer.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { IngredientService } from '../ingredient.service';
 import { Observable } from 'rxjs';
 import { Ingredient } from 'src/app/shared/models/ingredient.model';
+import { AngularFireAnalytics } from '@angular/fire/analytics';
 
 @Component({
   selector: 'app-ingredients-viewer',
@@ -14,11 +15,14 @@ export class IngredientsViewerComponent implements OnInit {
   @Output() onSelectedIngredient: EventEmitter<Ingredient> = new EventEmitter(null);
 
   constructor(
-    private ingredientService: IngredientService
+    private ingredientService: IngredientService,
+    private analytics: AngularFireAnalytics
   ) {}
 
   ngOnInit() {
     this.ingredients$ = this.ingredientService.ingredients$;
+
+    this.analytics.logEvent('page_view', { hello: 'Hello World!' })
   }
 
   onSelectIngredient(ingredient: Ingredient) {
--- src/app/features/orders/orders-list/orders-list.component.ts
+++ src/app/features/orders/orders-list/orders-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Hamburger } from 'src/app/shared/models/hamburger.model';
 import { HamburgerService } from '../../creator/hamburger.service';
 import { Observable } from 'rxjs';
+import { AngularFireAnalytics } from '@angular/fire/analytics';
 
 @Component({
   selector: 'app-orders-list',
@@ -10,14 +11,18 @@ import { Observable } from 'rxjs';
 })
 export class OrdersListComponent implements OnInit {
 
+  public burgerQuantity = 0;
   burgers$: Observable<Hamburger[]>;
 
   constructor(
-    private hamburgerService: HamburgerService
+    private hamburgerService: HamburgerService,
+    private analytics: AngularFireAnalytics
   ) {}
 
   ngOnInit() {
     this.burgers$ = this.hamburgerService.hamburgers$;
+    this.burgers$.subscribe(result => this.burgerQuantity = result.length);
+    this.analytics.logEvent('hamburgers_obtained', { quantity: this.burgerQuantity, module: 'order' });
   }
 
 }
